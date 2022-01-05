d00aca1f0c2ff09980e37c5cf193344680b35f0a
Raday <scdesignk@gmail.com>
2022-01-05 10:59:40 +0200

    added function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return String(n).substr(1).split("").reverse().join("");
+    }
+    return String(n).split("").reverse().join("");
+};
